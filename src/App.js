51c51d4135941c6dede352ae13c42e352faac9a3
Rasel <mdraselm413@gmail.com>
2022-09-26 18:09:03 +0600

    tablet added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Device from './Components/Device/Device';
+import Tablet from './Components/Tablet/Tablet';
 import Watch from './Components/Watch/Watch';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Device name='Iphone 14' price= '123456'></Device>
       <Watch></Watch>
+      <Tablet></Tablet>
     </div>
   );
 }
---
+++ src/Components/Tablet/Tablet.js
@@ -0,0 +1,15 @@
+
+
+import React, { Component } from 'react';
+
+class Tablet extends Component {
+    render() {
+        return (
+            <div>
+                <h3>This is Class Component With Tablet.</h3>
+            </div>
+        );
+    }
+}
+
+export default Tablet;
